f33ae28cb769634d5a6ef20ae500e43f33982da5
luckiest12 <lhn102@gmail.com>
2020-12-03 16:39:47 +0900

    #2

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
-
 const app = express()
 
-app.use(helmet());
+app.use( helmet());
 app.set('view engine', "pug");
 app.use(cookiePaser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
 app.use(morgan("dev"));
 app.use(localsMiddleware);
+app.use(function(req, res, next) {
+    res.setHeader("Content-Security-Policy", "script-src 'self' https://archive.org");
+    return next();
+    });
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
+import routes from "../routes";
+import { users, userDetail, chagePassword, editProfile } from '../controllers/userController';
 
 const userRouter = express.Router();
+
+userRouter.get(routes.changePassword, chagePassword);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.userDetail(), userDetail);
+
 
 export default userRouter;
