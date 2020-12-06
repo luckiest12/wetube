7381ba708b3c978c1783bc628540d064200306fd
luckiest12 <lhn102@gmail.com>
2020-12-06 19:45:38 +0900

    #4.2

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 
@@ -13,7 +14,7 @@ mongoose.connect(
 
 const db = mongoose.connection;
 const handleOpen = () => console.log("✅Connected to DB")
-const handleError = (error) => console.log('❌ Error on DB Conection:${error}')
+const handleError = error => console.log(`❌ Error on DB Conection:${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleError);
